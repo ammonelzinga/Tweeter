b33a32eb67ba39e30216894f2ba3bc3458d51776
ammonelzinga <ammonelzinga@gmail.com>
2025-03-08 22:14:48 -0700

    need postStatus and authentication for phse 2 still

--- tweeter-web/src/App.tsx
+++ tweeter-web/src/App.tsx
@@ -10,10 +10,16 @@ import Login from "./components/authentication/login/Login";
 import Register from "./components/authentication/register/Register";
 import MainLayout from "./components/mainLayout/MainLayout";
 import Toaster from "./components/toaster/Toaster";
-import { AuthToken, User, FakeData, Status } from "tweeter-shared";
+import { AuthToken, FakeData, Status } from "tweeter-shared";
 import UserItemScroller from "./components/mainLayout/UserItemScroller";
 import StatusItemScroller from "./components/mainLayout/StatusItemScroller";
 import useUserInfo from "./components/userInfo/UserInfoHook";
+import { UserItemView } from "./components/presenter/UserItemPresenter";
+import { FolloweePresenter } from "./components/presenter/FolloweePresenter";
+import { FollowerPresenter } from "./components/presenter/FollowerPresenter";
+import { FeedPresenter } from "./components/presenter/FeedPresenter";
+import { StatusItemView } from "./components/presenter/StatusItemPresenter";
+import { StoryPresenter } from "./components/presenter/StoryPresenter";
 
 const App = () => {
   const { currentUser, authToken } = useUserInfo();
@@ -37,45 +43,9 @@ const App = () => {
 };
 
 const AuthenticatedRoutes = () => {
-  const loadMoreFollowers = async (
-    authToken: AuthToken,
-    userAlias: string,
-    pageSize: number,
-    lastItem: User | null
-  ): Promise<[User[], boolean]> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
-  };
+ 
 
-  const loadMoreFollowees = async (
-    authToken: AuthToken,
-    userAlias: string,
-    pageSize: number,
-    lastItem: User | null
-  ): Promise<[User[], boolean]> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
-  };
-
-  const loadMoreStoryItems = async (
-      authToken: AuthToken,
-      userAlias: string,
-      pageSize: number,
-      lastItem: Status | null
-    ): Promise<[Status[], boolean]> => {
-      // TODO: Replace with the result of calling server
-      return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
-    };
-
-    const loadMoreFeedItems = async (
-      authToken: AuthToken,
-      userAlias: string,
-      pageSize: number,
-      lastItem: Status | null
-    ): Promise<[Status[], boolean]> => {
-      // TODO: Replace with the result of calling server
-      return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
-    };
+  
 
   return (
     <Routes>
@@ -86,8 +56,7 @@ const AuthenticatedRoutes = () => {
           element={
           <StatusItemScroller 
               key = {"feedScroller"}
-              loadItems={loadMoreFeedItems}
-              itemDescription="feed"
+              presenterGenerator={(view: StatusItemView) => new FeedPresenter(view)}
               />
             } 
           />
@@ -96,8 +65,7 @@ const AuthenticatedRoutes = () => {
           element={
             <StatusItemScroller
               key = {"storyScroller"}
-              loadItems={loadMoreStoryItems}
-              itemDescription="story"
+              presenterGenerator={(view: StatusItemView) => new StoryPresenter(view)}
             />
           } 
          />
@@ -106,8 +74,7 @@ const AuthenticatedRoutes = () => {
           element={
             <UserItemScroller
               key={1}
-              loadItems={loadMoreFollowees}
-                itemDescription="followees"
+              presenterGenerator={(view: UserItemView) => new FolloweePresenter(view)}
             />
           }
         />
@@ -116,8 +83,7 @@ const AuthenticatedRoutes = () => {
           element={
             <UserItemScroller
               key={2} 
-              loadItems={loadMoreFollowers}
-                itemDescription="followers"
+                presenterGenerator={(view: UserItemView) => new FollowerPresenter(view)}
             />
           }
         />
